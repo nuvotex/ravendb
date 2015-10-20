6bfa077cb2b3fa7cc7cb2ccf0256d5555d514125
Ayende Rahien <ayende@ayende.com>
2015-10-20 17:38:19 +0300

    Adding more tests Fixing position of elements in the FixedSizeTreeHeader
    Free space tests currently failing

--- Raven.Voron/Voron.Tests/Trees/CanIterateBackward.cs
+++ Raven.Voron/Voron.Tests/Trees/CanIterateBackward.cs
@@ -3,68 +3,79 @@ using Xunit;
 
 namespace Voron.Tests.Trees
 {
-	public class CanIterateBackward : StorageTest
-	{
-		[Fact]
-		public void SeekLastOnEmptyResultInFalse()
-		{
-			using (var tx = Env.NewTransaction(TransactionFlags.Read))
-			using (var it = tx.Root.Iterate())
-			{
-				Assert.False(it.Seek(Slice.AfterAllKeys));
+    public class CanIterateBackward : StorageTest
+    {
+        [Fact]
+        public void SeekLastOnEmptyResultInFalse()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.CreateTree("foo");
+                using (var it = tree.Iterate())
+                {
+                    Assert.False(it.Seek(Slice.AfterAllKeys));
 
-				tx.Commit();
-			}
-		}
+                    tx.Commit();
+                }
+            }
+        }
 
-		[Fact]
-		public void CanSeekLast()
-		{
-			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
-			{
-				tx.Root.Add("a", new MemoryStream(0));
-				tx.Root.Add("c", new MemoryStream(0));
-				tx.Root.Add("b", new MemoryStream(0));
+        [Fact]
+        public void CanSeekLast()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.CreateTree("foo");
+                tree.Add("a", new MemoryStream(0));
+                tree.Add("c", new MemoryStream(0));
+                tree.Add("b", new MemoryStream(0));
 
-				tx.Commit();
-			}
+                tx.Commit();
+            }
 
-			using (var tx = Env.NewTransaction(TransactionFlags.Read))
-			using (var it = tx.Root.Iterate())
-			{
-				Assert.True(it.Seek(Slice.AfterAllKeys));
-				Assert.Equal("c", it.CurrentKey.ToString());
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.CreateTree("foo");
+                using (var it = tree.Iterate())
+                {
+                    Assert.True(it.Seek(Slice.AfterAllKeys));
+                    Assert.Equal("c", it.CurrentKey.ToString());
 
-				tx.Commit();
-			}
-		}
+                    tx.Commit();
+                }
+            }
+        }
 
-		[Fact]
-		public void CanSeekBack()
-		{
-			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
-			{
-				tx.Root.Add("a", new MemoryStream(0));
-				tx.Root.Add("c", new MemoryStream(0));
-				tx.Root.Add			("b", new MemoryStream(0));
+        [Fact]
+        public void CanSeekBack()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.CreateTree("foo");
+                tree.Add("a", new MemoryStream(0));
+                tree.Add("c", new MemoryStream(0));
+                tree.Add("b", new MemoryStream(0));
 
-				tx.Commit();
-			}
+                tx.Commit();
+            }
 
-			using (var tx = Env.NewTransaction(TransactionFlags.Read))
-			using (var it = tx.Root.Iterate())
-			{
-				Assert.True(it.Seek(Slice.AfterAllKeys));
-				Assert.Equal("c", it.CurrentKey.ToString());
+            using (var tx = Env.ReadTransaction())
+            {
+                var tree = tx.ReadTree("foo");
+                using (var it = tree.Iterate())
+                {
+                    Assert.True(it.Seek(Slice.AfterAllKeys));
+                    Assert.Equal("c", it.CurrentKey.ToString());
 
-				Assert.True(it.MovePrev());
-				Assert.Equal("b", it.CurrentKey.ToString());
+                    Assert.True(it.MovePrev());
+                    Assert.Equal("b", it.CurrentKey.ToString());
 
-				Assert.True(it.MovePrev());
-				Assert.Equal("a", it.CurrentKey.ToString());
+                    Assert.True(it.MovePrev());
+                    Assert.Equal("a", it.CurrentKey.ToString());
 
-				tx.Commit();
-			}
-		}
-	}
+                    tx.Commit();
+                }
+            }
+        }
+    }
 }
--- Raven.Voron/Voron.Tests/Trees/Deletes.cs
+++ Raven.Voron/Voron.Tests/Trees/Deletes.cs
@@ -7,89 +7,97 @@ using Xunit;
 
 namespace Voron.Tests.Trees
 {
-	public class Deletes : StorageTest
-	{
+    public class Deletes : StorageTest
+    {
 
-		[Fact]
-		public void CanAddVeryLargeValueAndThenDeleteIt()
-		{
-			var random = new Random();
-			var buffer = new byte[8192];
-			random.NextBytes(buffer);
+        [Fact]
+        public void CanAddVeryLargeValueAndThenDeleteIt()
+        {
+            var random = new Random();
+            var buffer = new byte[8192];
+            random.NextBytes(buffer);
 
-            using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
-			{
-				tx.Root.Add			("a", new MemoryStream(buffer));
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.CreateTree("foo");
+                tree.Add("a", new MemoryStream(buffer));
 
-				tx.Commit();
-			}
+                tx.Commit();
+            }
 
-			using (var tx = Env.NewTransaction(TransactionFlags.Read))
-			{
-				Assert.Equal(4, tx.Root.State.PageCount);
-				Assert.Equal(3, tx.Root.State.OverflowPages);
-			}
+            using (var tx = Env.ReadTransaction())
+            {
+                var tree = tx.ReadTree("foo");
+                Assert.Equal(4, tree.State.PageCount);
+                Assert.Equal(3, tree.State.OverflowPages);
+            }
 
-			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
-			{
-				tx.Root.Delete("a");
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.ReadTree("foo");
+                tree.Delete("a");
 
-				tx.Commit();
-			}
-
-
-			using (var tx = Env.NewTransaction(TransactionFlags.Read))
-			{
-				Assert.Equal(1, tx.Root.State.PageCount);
-				Assert.Equal(0, tx.Root.State.OverflowPages);
-			}
-
-			using (var tx = Env.NewTransaction(TransactionFlags.Read))
-			{
-				Assert.Null(tx.Root.Read("a"));
-
-				tx.Commit();
-			}
-
-			
-		}
-
-		 [Fact]
-		 public void CanDeleteAtRoot()
-		 {
-             using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
-			 {
-				 for (int i = 0; i < 1000; i++)
-				 {
-					 tx.Root.Add			(string.Format("{0,5}",i), StreamFor("abcdefg"));
-				 }
-				 tx.Commit();
-			 }
-
-             var expected = new List<Slice>();
-             for (int i = 15; i < 1000; i++)
-             {
-                 expected.Add(string.Format("{0,5}", i));
-             }
-
-             using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
-			 {
-				 for (int i = 0; i < 15; i++)
-				 {
-					 tx.Root.Delete(string.Format("{0,5}", i));
-				 }
-				 tx.Commit();
-			 }
+                tx.Commit();
+            }
 
 
-             using (var tx = Env.NewTransaction(TransactionFlags.Read))
-			 {
-                 var list = Keys(tx.Root, tx);
-				 Assert.Equal(expected, list);
-			 }
-		 }
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.ReadTree("foo");
+                Assert.Equal(1, tree.State.PageCount);
+                Assert.Equal(0, tree.State.OverflowPages);
+            }
 
-        public unsafe List<Slice> Keys(Tree t, Transaction tx)
+            using (var tx = Env.ReadTransaction())
+            {
+                var tree = tx.ReadTree("foo");
+                Assert.Null(tree.Read("a"));
+
+                tx.Commit();
+            }
+
+
+        }
+
+        [Fact]
+        public void CanDeleteAtRoot()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.CreateTree("foo");
+                for (int i = 0; i < 1000; i++)
+                {
+                    tree.Add(string.Format("{0,5}", i), StreamFor("abcdefg"));
+                }
+                tx.Commit();
+            }
+
+            var expected = new List<Slice>();
+            for (int i = 15; i < 1000; i++)
+            {
+                expected.Add(string.Format("{0,5}", i));
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.ReadTree("foo");
+                for (int i = 0; i < 15; i++)
+                {
+                    tree.Delete(string.Format("{0,5}", i));
+                }
+                tx.Commit();
+            }
+
+
+            using (var tx = Env.ReadTransaction())
+            {
+                var tree = tx.ReadTree("foo");
+                var list = Keys(tree, tx);
+                Assert.Equal(expected, list);
+            }
+        }
+
+        public List<Slice> Keys(Tree t, Transaction tx)
         {
             var results = new List<Slice>();
             using (var it = t.Iterate())
@@ -103,5 +111,5 @@ namespace Voron.Tests.Trees
             }
             return results;
         }
-	}
+    }
 }
